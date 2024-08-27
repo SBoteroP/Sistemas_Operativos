576bc2b47aaef12fc6b37c6b4cbdf20f47f932cd
conconcon <19233029-conconcon@users.noreply.replit.com>
2024-08-27 16:24:32 +0000

    Automatización de la compilacion modular con un MakeFile

---
+++ Cap01/05_Parcial/library.c
@@ -0,0 +1,90 @@
+/* ****************************************************************
+Autor: Santiago Botero Pacheco
+Materia: Sistemas Operativos
+*******************************************************************
+Gestión de Memoria, Punteros, Compilación Modular |
+  Gestión de memoria (dinámica. estática), manipulación de
+  espacio de memoria según necesite la aplicación, por parte
+  del usuario. Se presenta la memoria estática que solo
+  se dispone en tiempo de compilación. Variables globales,
+  vectores fijos, etc. No es necesario liberar la memoria dinámica
+  que el ususario en función del problema puede cambiar, modificar,
+  el espacio de memoria que necesite. El tema a señalar, es que
+  se debe liberar la memoria al final.
+
+*******************************************************************
+*******************************************************************
+         IMPLEMENTACIÓN DE FUNCIONES DE LA BIBLIOTECA
+***************************************************************** */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "library.h"
+
+#define CAPACIDAD_INICIAL 4
+
+/*Crea un nuevo vector dinámico con la capacidad inicial definida*/
+void vectorInicio(vectorDinamico *V) {
+  // Inicializa la capacidad del vector a la capacidad inicial definida.
+  V->capacidad = CAPACIDAD_INICIAL;
+  // Inicializa el número total de elementos del vector a 0.
+  V->totalElementos = 0;
+  // Asigna memoria para el arreglo de elementos del vector.
+  V->elementos = malloc(sizeof(void *) * V->capacidad);
+}
+
+/*Devuelve el número total de elementos en el vector*/
+int totalVector(vectorDinamico *V) { return V->totalElementos; }
+
+/*Redimensiona el vector para una nueva capacidad*/
+static void resizeVector(vectorDinamico *V, int capacidad) {
+  printf("Redimensión: %d a %d \n", V->capacidad, capacidad);
+
+  void **elementos = realloc(V->elementos, sizeof(void *) * capacidad);
+  if (elementos) {
+    V->elementos = elementos;
+    V->capacidad = capacidad;
+  }
+}
+
+/*Agrega un nuevo elemento al vector*/
+void addVector(vectorDinamico *V, void *elemento) {
+  if (V->capacidad == V->totalElementos)
+    resizeVector(V, V->capacidad * 2);
+  V->elementos[V->totalElementos++] = elemento;
+}
+
+/*Libera la memoria asignada al vector*/
+void freeVector(vectorDinamico *V) { free(V->elementos); }
+
+/*Devuelve el elemento en la posición indicada del vector*/
+void *getVector(vectorDinamico *V, int indice) {
+  if (indice >= 0 && indice < V->totalElementos)
+    return V->elementos[indice];
+  return NULL;
+}
+
+/*Asigna un nuevo valor al elemento en la posición indicada del vector*/
+void setVector(vectorDinamico *V, int indice, void *elemento) {
+  if (indice >= 0 && indice < V->totalElementos)
+    V->elementos[indice] = elemento;
+}
+
+/*Elimina el elemento en la posición indicada del vector*/
+void borrarVector(vectorDinamico *V, int indice) {
+  if (indice < 0 || indice >= V->totalElementos)
+    return;
+
+  V->elementos[indice] = NULL;
+
+  for (int i = indice; i < V->totalElementos - 1; i++) {
+    V->elementos[i] = V->elementos[i + 1];
+    V->elementos[i + 1] = NULL;
+  }
+  V->totalElementos--;
+  if (V->totalElementos > 0 && V->totalElementos == V->capacidad / 4)
+    resizeVector(V, V->capacidad / 2);
+}
+
--- Cap01/05_Parcial/memModular_Botero.c
+++ Cap01/05_Parcial/memModular_Botero.c
@@ -12,84 +12,18 @@ Gestión de Memoria, Punteros, Compilación Modular |
   que el ususario en función del problema puede cambiar, modificar,
   el espacio de memoria que necesite. El tema a señalar, es que
   se debe liberar la memoria al final.
-
+Compilación Modular: Se divide el programa en módulos, cada uno
+  con una función principal. Se puede compilar el programa en
+  varios módulos, cada uno con una función principal.
 ***************************************************************** */
 
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "library.h"
+
 #define CAPACIDAD_INICIAL 4
-
-/*Creación Estructura de Datos para el Vector*/
-typedef struct vectorDinamico {
-  int capacidad;
-  int totalElementos;
-  void **elementos;
-} vectorDinamico;
-
-/*Crea un nuevo vector dinámico con la capacidad inicial definida*/
-void vectorInicio(vectorDinamico *V) {
-  // Inicializa la capacidad del vector a la capacidad inicial definida.
-  V->capacidad = CAPACIDAD_INICIAL;
-  // Inicializa el número total de elementos del vector a 0.
-  V->totalElementos = 0;
-  // Asigna memoria para el arreglo de elementos del vector.
-  V->elementos = malloc(sizeof(void *) * V->capacidad);
-}
-
-/*Devuelve el número total de elementos en el vector*/
-int totalVector(vectorDinamico *V) { return V->totalElementos; }
-
-/*Redimensiona el vector para una nueva capacidad*/
-static void resizeVector(vectorDinamico *V, int capacidad) {
-  printf("Redimensión: %d a %d \n", V->capacidad, capacidad);
-
-  void **elementos = realloc(V->elementos, sizeof(void *) * capacidad);
-  if (elementos) {
-    V->elementos = elementos;
-    V->capacidad = capacidad;
-  }
-}
-
-/*Agrega un nuevo elemento al vector*/
-void addVector(vectorDinamico *V, void *elemento) {
-  if (V->capacidad == V->totalElementos)
-    resizeVector(V, V->capacidad * 2);
-  V->elementos[V->totalElementos++] = elemento;
-}
-
-/*Libera la memoria asignada al vector*/
-void freeVector(vectorDinamico *V) { free(V->elementos); }
-
-/*Devuelve el elemento en la posición indicada del vector*/
-void *getVector(vectorDinamico *V, int indice) {
-  if (indice >= 0 && indice < V->totalElementos)
-    return V->elementos[indice];
-  return NULL;
-}
-
-/*Asigna un nuevo valor al elemento en la posición indicada del vector*/
-void setVector(vectorDinamico *V, int indice, void *elemento) {
-  if (indice >= 0 && indice < V->totalElementos)
-    V->elementos[indice] = elemento;
-}
-
-/*Elimina el elemento en la posición indicada del vector*/
-void borrarVector(vectorDinamico *V, int indice) {
-  if (indice < 0 || indice >= V->totalElementos)
-    return;
-
-  V->elementos[indice] = NULL;
-
-  for (int i = indice; i < V->totalElementos - 1; i++) {
-    V->elementos[i] = V->elementos[i + 1];
-    V->elementos[i + 1] = NULL;
-  }
-  V->totalElementos--;
-  if (V->totalElementos > 0 && V->totalElementos == V->capacidad / 4)
-    resizeVector(V, V->capacidad / 2);
-}
 
 int main() {
   printf("\n");
