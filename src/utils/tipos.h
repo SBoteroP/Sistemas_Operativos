9a09b349a56c9b20babac0aac4bcf194ca272598
SANTIAGO BOTERO PACHECO <68749776+SBoteroP@users.noreply.github.com>
2024-10-15 01:26:24 -0500

    Add files via upload

---
+++ src/utils/tipos.h
@@ -0,0 +1,16 @@
+#ifndef TIPOS_H
+#define TIPOS_H
+
+#define MAX_NEWS_LENGTH 80
+
+typedef enum {
+  Arte = 'A';
+  Espectaculos = 'E';
+  Ciencia = 'C';
+  Politica = 'P';
+  Sucesos = 'S';
+} Categoria;
+
+typedef struct {
+  
+}
